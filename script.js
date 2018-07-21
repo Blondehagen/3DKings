eddae8db543e082e128d0ff0d96ba7d422f48ada
Kristin Bondehagen <39173402+Blondehagen@users.noreply.github.com>
2018-07-21 14:22:51 -0400

    Added script file and updated css

---
+++ script.js
@@ -0,0 +1,7 @@
+const nameInput = document.getElementById('name');
+const messageTextArea = document.getElementById('message');
+
+nameInput.addEventListener('click', event => {
+    event.target.className = 'highlight';
+    
+});
