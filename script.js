bf3b7f475fdad26c8a812554968131d2677baa1d
Kristin Bondehagen <39173402+Blondehagen@users.noreply.github.com>
2018-07-26 19:14:33 -0400

    Added comment about code

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+//Form Field
+
 const nameInput = document.getElementById('name');
 const messageTextArea = document.getElementById('message');
 
 nameInput.addEventListener('click', event => {
     event.target.className = 'highlight';
-    
-});
+    });
